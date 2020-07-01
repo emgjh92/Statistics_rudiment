6ac2d1575d16d59f8ab370ac954540eb8432921f
Choi jaehyun <emgjh92@naver.com>
2020-07-01 10:18:37 +0900

    2020-07-01
    2020-07-01

--- 20200630/race_craw.py
+++ 20200630/race_craw.py
@@ -1,20 +1,16 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Tue Jun 30 12:11:21 2020
-
-@author: chjh9
-"""
-
 
 import requests
 import bs4
 import csv
 import time
 import datetime
+import random
 
 race_number =1
 race_date = datetime.datetime(2017,7,23)
-end_date = datetime.datetime(2020,6,1)
+end_date = datetime.datetime(2017,8,10)
+
+dataList = []
 
 while race_date < end_date:
     
@@ -24,10 +20,33 @@ while race_date < end_date:
         
         
         #파싱...저장....
-        print(race_date)
+        # print(race_date)
+        soup = bs4.BeautifulSoup(html,"html.parser")
+        tables = soup.select("#contents .tableType2 table")
+        #print("테이블 갯수 : " , len(tables))
+        trList = tables[0].select("tr")
         
         
-        time.sleep(1)
-    
+        for tr in trList:
+            tdList = tr.select("td")
+            
+            row = []
+            
+            for td in tdList:
+                #print(td.text)
+                row.append(td.text)
+            
+            dataList.append(row)
+        
+      
+    time.sleep(random.randint(100,500)/1000)
    
     race_date = race_date + datetime.timedelta(days=7)
+    
+    
+print("완료!!!!")
+#print(dataList)
+fd = open("./race.csv","w",newline="",encoding='utf-8')
+csvfile = csv.writer(fd)
+csvfile.writerows(dataList)
+fd.close()
